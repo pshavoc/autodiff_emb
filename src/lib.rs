87b35d70168195fc4c16e62dfcf472039dda30bb
Paul Soucy <paul.soucy@havocai.com>
2025-12-17 16:45:01 -0500

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,30 @@ extern crate alloc;
 mod forward_autodiff;
 pub use forward_autodiff::*;
 
+pub fn jacobian<'a, G, T, F, const N: usize, const M: usize, O>(
+    g: G,
+    x: nalgebra::VectorView<'a, Dual<T>, nalgebra::Const<N>>,
+) -> nalgebra::SMatrix<T, N, N>
+where
+    G: FnOnce(
+        nalgebra::VectorView<'a, Dual<T>, nalgebra::Const<N>>,
+    ) -> nalgebra::OVector<Dual<T>, nalgebra::Const<M>>,
+    T: DualNumFloat,
+{
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    #[test]
+    fn test_derivative() {
+        let f = |x: Dual32, y: Dual32| -> Dual32 { x * x + y };
+
+        let x = f(Dual32::from_re(3.0).derivative(), Dual32::from_re(3.0));
+
+        assert_eq!(x.re, 12.0);
+        assert_eq!(x.eps, 6.0);
+    }
+}
